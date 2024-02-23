84bc773cfd760edaf08f29c3842c794bd6397919
droopyantidote <144815313+droopyantidote@users.noreply.github.com>
2024-02-23 12:14:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const parseCode = (str) => {
-  // your code here
+  // Find the indexes of zeros in the string
+  const zeroIndexes = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '0') {
+      zeroIndexes.push(i);
+    }
+  }
+
+  // Extract firstName, lastName, and id using the zero indexes
+  const firstName = str.substring(0, zeroIndexes[0]);
+  const lastName = str.substring(zeroIndexes[0] + 3, zeroIndexes[1]);
+  const id = str.substring(zeroIndexes[1] + 3);
+
+  // Return an object with properties firstName, lastName, and id
+  return {
+    firstName: firstName,
+    lastName: lastName,
+    id: id
+  };
 };
 
 // Do not change the code below
